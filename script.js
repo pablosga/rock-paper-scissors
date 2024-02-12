0b3cb2a229b54e017f8709eb85d8f850207ff7c9
pablosga <pablosgabussi123@gmail.com>
2024-02-12 07:01:09 -0300

    Creacion de la funcion getComputerChoice
    Permitira que la maquina elija entre las tres opciones rock, paper, scissors aleatoriamente

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("hello world")
+function getComputerChoice() {
+    let options = ["rock", "paper", "scissors"];
+    let randomPosition = Math.floor(Math.random() * 3);
+    return options[randomPosition];
+}
