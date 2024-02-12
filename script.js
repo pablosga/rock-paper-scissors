13f8170ddb83b11ea5082327f80e8acd5cb6f088
pablosga <pablosgabussi123@gmail.com>
2024-02-12 07:09:10 -0300

    Crea la funcion getPlayerChoice
    La funcion pide al usuario que elija rock, papaer o scissors

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ function getComputerChoice() {
     let randomPosition = Math.floor(Math.random() * 3);
     return options[randomPosition];
 }
+
+function getPlayerChoice() {
+    let playerChoice = prompt("Rock, Paper or Scissors?")
+    return playerChoice.toLowerCase();
+}
+
+
+
+
+
