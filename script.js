917dd418237f10c0d6b1ba7edf740cd60e5650b1
pablosga <pablosgabussi123@gmail.com>
2024-02-12 07:33:10 -0300

    Crea la funcion playRound
    Toma como parametros la eleccion de la maquina y la eleccion del jugador y retorna el resultado del juego

--- script.js
+++ script.js
@@ -9,6 +9,39 @@ function getPlayerChoice() {
     return playerChoice.toLowerCase();
 }
 
+function playRound(getComputerChoice, getPlayerChoice) {
+    switch (getPlayerChoice) {
+        case "rock":
+            if (getComputerChoice === "rock") {
+                return "Empate! Ambos elijieron piedra"
+            } else if (getComputerChoice === "paper") {
+                return "Perdiste! Papel vence a Piedra"
+            } else {
+                return "Ganaste! Piedra vence a Tijeras"
+            }
+            break;
+
+        case "paper":
+            if (getComputerChoice === "rock") { 
+                return "Ganaste! Papel vence a Piedra"
+            } else if (getComputerChoice === "paper") {
+                return "Empate! ambos elijieron papel"
+            } else {
+                return "Perdiste! Tijeras vence a Papel"
+            }
+            break;
+
+        case "scissors":
+            if (getComputerChoice === "rock") {
+                return "Perdiste! Piedra vence a Tijeras"
+            } else if (getComputerChoice === "paper") {
+                return "Ganaste! Tijeras vence a Papel"
+            } else {
+                return "Empate! ambos elijieron Tijeras"
+            }
+            break;
+    }
+}
 
 
 
